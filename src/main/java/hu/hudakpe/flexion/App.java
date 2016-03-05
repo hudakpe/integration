0bb53b09d6de274423f0ae33dfe24acd7df52b52
hudak <hudakpe@gmail.com>
2016-03-05 15:53:47 +0100

    Add my name to the main method

--- src/main/java/hu/hudakpe/flexion/App.java
+++ src/main/java/hu/hudakpe/flexion/App.java
@@ -8,7 +8,7 @@ public class App {
     public static void main(String[] args) {
 
         System.out.println("Running tests is started...");
-        Integration defaultIntegration = new DefaultIntegration();
+        Integration defaultIntegration = new DefaultIntegration("peterhudak");
         IntegrationTestRunner testRunner = new IntegrationTestRunner();
         testRunner.runTests(defaultIntegration);
         System.out.println("Running tests is finished...");
